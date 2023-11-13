8d3ca0506eb3b40f8223ebbf5b295569474a4e42
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-13 15:11:07 +0100

    dati 3 id agli input in index.html

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// 
+
+
+console.log('ciaooo')
+
+// const kmDaPercorrere = parseInt(prompt('Quanti km vuoi percorrere?'));
+// const etaPasseggero = parseInt(prompt('Quanti anni ha il passeggero?'));
+
+const kmDaPercorrere = 20;
+const etaPasseggero = 30;
+
+let prezzoViaggio = kmDaPercorrere * 0.1976;
+
+console.log(prezzoViaggio)
+
+// if (etaPasseggero < 18) {
+//     const sconto = prezzoViaggio * 20 / 100;
+//     // prezzoViaggio = prezzoViaggio - sconto;
+//     prezzoViaggio -= sconto;
+//     console.log(sconto,prezzoViaggio);
+// } else if (etaPasseggero > 65) {
+//     const sconto = prezzoViaggio * 40 / 100;
+//     prezzoViaggio -= sconto;
+//     console.log(sconto,prezzoViaggio)
+// } else {
+//     console.log(prezzoViaggio)
+// }
+
+// prezzoViaggio = prezzoViaggio.toFixed(2)
+// console.log(prezzoViaggio)
+
+// document.getElementById('prezzo-biglietto').innerHTML = prezzoViaggio
