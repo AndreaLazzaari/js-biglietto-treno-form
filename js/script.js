4f923c0ba3dbeb4aacc08264d05b2dfddfca76db
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-13 16:27:02 +0100

    inserito all'interno della function condizione età passeggero inferiore 18 o maggiore 65

--- js/script.js
+++ js/script.js
@@ -5,20 +5,32 @@ console.log('ciaooo')
 
 let buttonElement = document.querySelector ('button');
 
-let prezzoViaggio = kmDaPercorrere * 0.1976;
+
 
 buttonElement.addEventListener('click', function() {
     console.log('inizio per il calcolo prezzo');
-    const kmDaPercorrere = document.getElementById('km-percorrere');
-    let kmDaPercorrere = parseFloat(kmDaPercorrere.innerHTML)
-
-    const etaPasseggero = document.getElementById('eta-passeggero');
-    let etaPasseggero = parseInt(etaPasseggero.innerHTML)
-
+    let kmDaPercorrere = parseFloat(document.getElementById('km-percorrere').value);
+    let etaPasseggero = parseInt(document.getElementById('eta-passeggero').value);
+    
     let prezzoViaggio = kmDaPercorrere * 0.1976;
 
+    console.log(kmDaPercorrere, etaPasseggero, prezzoViaggio);
 
-
-
+    if (etaPasseggero < 18) {
+        const sconto = prezzoViaggio * 17.65 / 100;
+        // prezzoViaggio = prezzoViaggio - sconto;
+        prezzoViaggio -= sconto;
+        console.log(sconto,prezzoViaggio);
+    } else if (etaPasseggero >= 65) {
+        const sconto = prezzoViaggio * 53.27 / 100;
+        prezzoViaggio -= sconto;
+        console.log(sconto,prezzoViaggio)
+    } else {
+        console.log(prezzoViaggio)
+    }
+    
+    console.log('fine calcolo viaggio');
+    prezzoViaggio = prezzoViaggio.toFixed(2);
+    console.log(prezzoViaggio);
 
 })
